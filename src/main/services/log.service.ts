edaf92228bc7de72700bf856b5d508bc73b05e4c
ArisAgeha <87725092@qq.com>
2020-05-07 10:19:21 +0800

    remove test code

--- src/main/services/log.service.ts
+++ src/main/services/log.service.ts
@@ -8,11 +8,6 @@ export class LogService {
     constructor() {}
 
     log(message: any): void {
-        // if (isObject) message = JSON.stringify(message, null, 4);
-        // console.log(message);
-        // const p = path.resolve('C:/Users/87725/Desktop/debugger/log.txt');
-        // if (fs.existsSync(p)) fs.appendFileSync(p, `\r\n\r\n${message}`);
-        // else fs.writeFileSync(p, message);
     }
 
     warn(message: any): void {
