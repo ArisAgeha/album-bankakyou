cfb0154fba07bed4cab31a994c56997fe93acd2f
ArisAgeha <87725092@qq.com>
2020-05-10 09:40:02 +0800

    add auto update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { app, BrowserWindow, Menu } from 'electron';
 import { CodeMain } from './main/code/main';
-import { isDev } from './common/utils/functionTools';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 declare const WORKER_WINDOW_WEBPACK_ENTRY: any;
+require('update-electron-app')();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
